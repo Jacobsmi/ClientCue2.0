c76258d0c3e27c288070ee980a661aad293bdaf7
Jacobsmi <Jacobsm84@gmail.com>
2021-06-01 11:42:36 -0400

    Changed to grid for layout

--- client/src/pages/signup/Signup.js
+++ client/src/pages/signup/Signup.js
@@ -1,9 +1,15 @@
 import './Signup.css'
+import arrived from './Arrived.svg'
 
 export default function Signup(){
   return(
     <div className='Signup'>
-
+      <div className='SignupPanel'>
+        Sign Up
+      </div>
+      <div className='ImagePanel'>
+      <img src={arrived} className='SignupImage' alt='Sign Up Image' />
+      </div>
     </div>
   )
 }
