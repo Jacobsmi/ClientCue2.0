d6ea6cd043abc06f939b4bb513699ba3f53a68fc
Jacobsmi <jacobsm84@gmail.com>
2021-06-01 10:13:24 -0400

    Created Signup Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
-import Home from './pages/home/Home.js';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Home from './pages/home/Home.js'; 
+import Signup from './pages/signup/Signup.js';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Switch>
+          
+          <Route path='/signup'>
+            <Signup />
+          </Route>
+
           <Route exact path='/'>
             <Home />
           </Route>
+
         </Switch>
       </BrowserRouter>
     </div>
