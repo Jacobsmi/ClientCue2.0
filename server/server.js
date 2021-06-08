875d66a86f291ace5dac911396a0f191bca2ee91
Jacobsmi <jacobsm84@gmail.com>
2021-06-07 22:31:59 -0400

    Signup route started

--- server/server.js
+++ server/server.js
@@ -1,14 +1,22 @@
 const express = require("express")
 
 const app = express()
+app.use(express.json())
 
-const port = 3000;
+
+const port = 5000;
 
 app.get('/', (req, res) => {
   res.send("Hello World")
 })
 
-
+app.post("/signup", (req, res)=>{
+  if(!req.body.firstName){
+    res.send("No first name")
+  }else{
+    res.send("Info present")
+  }
+})
 app.listen(port, ()=>{
   console.log(`Server is running on http://localhost:${port}`)
 })
