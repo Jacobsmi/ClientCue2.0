1ff897e93c57df5777982d378ea9af84daff066f
Jacobsmi <jacobsm84@gmail.com>
2021-06-08 15:57:02 -0400

    Error handling for signup route created

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.post("/signup", async (req, res)=>{
   // Check to see if all information is present in the body of the POST request
   if(!req.body.firstName || !req.body.lastName || !req.body.email || !req.body.password){
     // If info is missing send a 400 error
-    res.status(400).send(JSON.stringify({success: false, msg: "missing info"}))
+    return res.status(400).send(JSON.stringify({success: false, msg: "missing info"}))
   }
   // If all information is present attempt to insert into the database
   else{
@@ -34,11 +34,26 @@ app.post("/signup", async (req, res)=>{
     }
     // Catch and report any errors
     catch(err){
-      console.log(err)
-      res.status(500).send(JSON.stringify({success: false, msg: "error inserting into db"}))
+      // Get the error as a string for parsing
+      const errString = err.toString()
+
+      // Check if the error is a table not exist error in which case the DB probably hasn't been set up/configuration failed
+      if (errString.includes("no such table")){
+        return res.status(500).send(JSON.stringify({success: false, msg: "db not properly configured"}))
+      }
+      // Catch non-unique email
+      else if(errString.includes("SequelizeUniqueConstraintError")){
+        return res.status(500).send(JSON.stringify({success: false, msg: "email not unqiue"}))
+      }
+      // Handle all other errors
+      else{
+        console.log(`Error Message: ${err.toString()}`)
+        console.log(err)
+        return res.status(500).send(JSON.stringify({success: false, msg: "unhandled error inserting into db"}))
+      }
     }
     // If everything goes well send response
-    res.status(200).send(JSON.stringify({success: true}))
+    return res.status(200).send(JSON.stringify({success: true}))
   }
 })
 app.listen(port, ()=>{
